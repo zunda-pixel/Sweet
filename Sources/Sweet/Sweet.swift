286dad8df413e7b8edd98b37915a62fd80cc59fd
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-01-15 20:43:55 +0900

    Create Sweet.swift

---
+++ Sources/Sweet/Sweet.swift
@@ -0,0 +1,17 @@
+//
+//  Sweet.swift
+//
+//
+//  Created by zunda on 2022/01/14.
+//
+
+@available(macOS 12.0, *)
+public struct Sweet {
+ let consumerKey: String
+ let consumerSecretKey: String
+
+ let oauthToken: String
+ let oauthSecretToken: String
+
+ let bearerToken: String
+}
