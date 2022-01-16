f624f5d690ede2035889b6c546191a6a26d2ed99
zunda-pixel <47569369+zunda-pixel@users.noreply.github.com>
2022-01-17 00:53:09 +0900

    HTTPClientを綺麗に

--- Sources/Sweet/HTTPClient.swift
+++ Sources/Sweet/HTTPClient.swift
@@ -5,6 +5,37 @@
 //  Created by zunda on 2022/01/14.
 //
 
+import Foundation
+
+struct HTTPClient {
+  private static let timeout: Double = 60.0
+  
+  public static func request(method: HTTPMethod, url: URL, body: Data? = nil, headers: [String: String] = [:], queries: [String: String] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+    var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)!
+    urlComponents.queryItems = queries.map { .init(name: $0, value: $1)}
+    
+    var request: URLRequest = .init(url: urlComponents.url!)
+    request.httpMethod = method.rawValue
+    request.allHTTPHeaderFields = headers
+    request.httpBody = body
+    request.timeoutInterval = timeout
+    
+    return try await URLSession.shared.data(for: request)
+  }
+  
+  public static func get(url: URL, body: Data? = nil, headers: [String: String] = [:], queries: [String: String] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+    return try await request(method: .GET, url: url, body: body, headers: headers, queries: queries, timeout: timeout)
+  }
+  
+  public static func post(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+    return try await request(method: .POST, url: url, body: body, headers: headers, queries: queries, timeout: timeout)
+  }
+  
+  public static func delete(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
+    return try await request(method: .DELETE, url: url, body: body, headers: headers, queries: queries, timeout: timeout)
+  }
+}
+
 @available(iOS, introduced: 13.0, deprecated: 15.0, message: "Use the built-in API instead")
 extension URLSession {
   func data(from url: URL) async throws -> (Data, URLResponse) {
@@ -36,8 +67,6 @@ extension URLSession {
   }
 }
 
-import Foundation
-
 public enum HTTPMethod : String {
   case GET
   case HEAD
@@ -48,33 +77,4 @@ public enum HTTPMethod : String {
   case OPTIONS
   case TRACE
   case PATCH
-}
-
-struct HTTPClient {
-  private static let timeout: Double = 60.0
-  
-  public static func request(method: HTTPMethod, url: URL, body: Data? = nil, headers: [String: String] = [:], queries: [String: String] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
-    var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)!
-    urlComponents.queryItems = queries.map { .init(name: $0, value: $1)}
-    
-    var request: URLRequest = .init(url: urlComponents.url!)
-    request.httpMethod = method.rawValue
-    request.allHTTPHeaderFields = headers
-    request.httpBody = body
-    request.timeoutInterval = timeout
-    
-    return try await URLSession.shared.data(for: request)
-  }
-  
-  public static func get(url: URL, body: Data? = nil, headers: [String: String] = [:], queries: [String: String] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
-    return try await request(method: .GET, url: url, body: body, headers: headers, queries: queries, timeout: timeout)
-  }
-  
-  public static func post(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
-    return try await request(method: .POST, url: url, body: body, headers: headers, queries: queries, timeout: timeout)
-  }
-  
-  public static func delete(url: URL, body: Data? = nil , headers: [String: String] = [:], queries: [String: String] = [:], timeout: Double = timeout) async throws -> (Data, URLResponse) {
-    return try await request(method: .DELETE, url: url, body: body, headers: headers, queries: queries, timeout: timeout)
-  }
 }
