2cd32224f7a9c65e5ffe12711cbfe724fc0682d9
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-01-17 16:36:06 +0900

    pinList, unPinList, fetchPinnedListsを実装

--- Sources/Sweet/Model/ListModel.swift
+++ Sources/Sweet/Model/ListModel.swift
@@ -43,3 +43,21 @@ struct MemberResponseModel: Decodable {
     self.isMember = try usersInfo.decode(Bool.self, forKey: .isMember)
   }
 }
+
+struct PinResponseModel: Decodable {
+  let pinned: Bool
+  
+  private enum DataCodingKeys: String, CodingKey {
+    case data = "data"
+  }
+  
+  private enum CodingKeys: String, CodingKey {
+    case pinned
+  }
+  
+  public init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: DataCodingKeys.self)
+    let usersInfo = try values.nestedContainer(keyedBy: CodingKeys.self, forKey: .data)
+    self.pinned = try usersInfo.decode(Bool.self, forKey: .pinned)
+  }
+}
