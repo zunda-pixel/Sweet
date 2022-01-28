4379627e2cd1e2ff358974d9b3c4b4ef6ad36e38
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-01-29 00:01:25 +0900

    Update Sweet.swift

--- Sources/Sweet/Sweet.swift
+++ Sources/Sweet/Sweet.swift
@@ -5,13 +5,15 @@
 //  Created by zunda on 2022/01/14.
 //
 
-@available(macOS 12.0, *)
 public struct Sweet {
- let consumerKey: String
- let consumerSecretKey: String
-
- let oauthToken: String
- let oauthSecretToken: String
-
- let bearerToken: String
+  let bearerTokenApp: String
+  let bearerTokenUser: String
+  
+  static func exampleSweet() -> Sweet {
+    let bearerTokenUser = ""
+    let bearerTokenApp = ""
+    let sweet = Sweet(bearerTokenApp: bearerTokenApp, bearerTokenUser: bearerTokenUser)
+    
+    return sweet
+  }
 }
