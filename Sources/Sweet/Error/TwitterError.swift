7ebb94ab85e838fbfbd8c68ddcfe1296a4730a92
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-03-14 22:56:37 +0900

    Twitterエラーを実装

---
+++ Sources/Sweet/Error/TwitterError.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by zunda on 2022/03/14.
+//
+
+import Foundation
+
+public enum TwitterError: Error {
+  case invalidRequest(error: ResponseErrorModel)
+  case unknwon(data: Data, response: URLResponse)
+}
