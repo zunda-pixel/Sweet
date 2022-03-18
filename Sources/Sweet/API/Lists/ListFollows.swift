330e66f4a8808aee9abee6f8cebefbd44e04a698
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-03-18 15:32:29 +0900

    follow, unfollowは戻り値は返さないで返ってきたデータで判断してthrowするように

--- Sources/Sweet/API/Lists/ListFollows.swift
+++ Sources/Sweet/API/Lists/ListFollows.swift
@@ -9,49 +9,53 @@ import Foundation
 import HTTPClient
 
 extension Sweet {
-  public func unFollowList(userID: String, listID: String) async throws -> Bool {
+  public func unFollowList(userID: String, listID: String) async throws {
     // https://developer.twitter.com/en/docs/twitter-api/lists/list-follows/api-reference/delete-users-id-followed-lists-list_id
-
+    
     let url: URL = .init(string: "https://api.twitter.com/2/users/\(userID)/followed_lists/\(listID)")!
-
+    
     let headers = getBearerHeaders(type: .User)
     
     let (data, urlResponse) = try await HTTPClient.delete(url: url, headers: headers)
-            
+    
     if let response = try? JSONDecoder().decode(UnFollowResponseModel.self, from: data) {
-      return response.following
+      if response.following {
+        throw TwitterError.followError
+      }
     }
-        
+    
     if let response = try? JSONDecoder().decode(ResponseErrorModel.self, from: data) {
-          throw TwitterError.invalidRequest(error: response)
-        }
-        
-        throw TwitterError.unknwon(data: data, response: urlResponse)
+      throw TwitterError.invalidRequest(error: response)
+    }
+    
+    throw TwitterError.unknwon(data: data, response: urlResponse)
   }
-
-  public func followList(userID: String, listID: String) async throws -> Bool {
+  
+  public func followList(userID: String, listID: String) async throws {
     // https://developer.twitter.com/en/docs/twitter-api/lists/list-follows/api-reference/post-users-id-followed-lists
-
+    
     let url: URL = .init(string: "https://api.twitter.com/2/users/\(userID)/followed_lists")!
     
     let body = ["list_id": listID]
     let bodyData = try JSONEncoder().encode(body)
-
+    
     let headers = getBearerHeaders(type: .User)
     
     let (data, urlResponse) = try await HTTPClient.post(url: url, body: bodyData, headers: headers)
     
     if let response = try? JSONDecoder().decode(UnFollowResponseModel.self, from: data) {
-      return response.following
+      if !response.following {
+        throw TwitterError.followError
+      }
     }
     
     if let response = try? JSONDecoder().decode(ResponseErrorModel.self, from: data) {
-          throw TwitterError.invalidRequest(error: response)
-        }
-        
-        throw TwitterError.unknwon(data: data, response: urlResponse)
+      throw TwitterError.invalidRequest(error: response)
+    }
+    
+    throw TwitterError.unknwon(data: data, response: urlResponse)
   }
-
+  
   public func fetchFollowedUsers(listID: String, maxResults: Int = 100, paginationToken: String? = nil,
                                  userFields: [UserField] = [], tweetFields: [TweetField] = []) async throws -> [UserModel] {
     // https://developer.twitter.com/en/docs/twitter-api/lists/list-follows/api-reference/get-lists-id-followers
@@ -64,11 +68,11 @@ extension Sweet {
       UserField.key: tweetFields.map(\.rawValue).joined(separator: ","),
       TweetField.key: tweetFields.map(\.rawValue).joined(separator: ","),
     ].filter { $0.value != nil }
-
+    
     let headers = getBearerHeaders(type: .User)
     
     let (data, urlResponse) = try await HTTPClient.get(url: url, headers: headers, queries: queries)
-            
+    
     if let response = try? JSONDecoder().decode(UsersResponseModel.self, from: data) {
       return response.users
     }
@@ -79,7 +83,7 @@ extension Sweet {
     
     throw TwitterError.unknwon(data: data, response: urlResponse)
   }
-
+  
   public func fetchFollowingLists(userID: String, maxResults: Int = 100, paginationToken: String? = nil,
                                   listFields: [ListField] = [], userFields: [UserField] = []) async throws -> [ListModel] {
     // https://developer.twitter.com/en/docs/twitter-api/lists/list-follows/api-reference/get-users-id-followed_lists
@@ -96,11 +100,11 @@ extension Sweet {
     let headers = getBearerHeaders(type: .User)
     
     let (data, urlResponse) = try await HTTPClient.get(url: url, headers: headers, queries: queries)
-        
+    
     if let response = try? JSONDecoder().decode(ListsResponseModel.self, from: data) {
       return response.lists
     }
-        
+    
     if let response = try? JSONDecoder().decode(ResponseErrorModel.self, from: data) {
       throw TwitterError.invalidRequest(error: response)
     }
