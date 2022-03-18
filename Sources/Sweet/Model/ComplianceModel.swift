750785da432e3de3a13478bb49b35bccd3dcf742
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-03-18 13:28:42 +0900

    Complianceのnameをdecodeするように

--- Sources/Sweet/Model/ComplianceModel.swift
+++ Sources/Sweet/Model/ComplianceModel.swift
@@ -14,6 +14,7 @@ public enum JobType: String {
 
 public struct ComplianceModel {
   public let type: JobType
+  public let name: String
   public let id: String
   public let resumble: Bool
   public let uploadURL: URL
@@ -23,11 +24,12 @@ public struct ComplianceModel {
   public let createdAt: Date
   public let status: String
   
-  public init(type: JobType, id: String, resumble: Bool, uploadURL: URL,
+  public init(type: JobType, id: String, name: String, resumble: Bool, uploadURL: URL,
               uploadExpiresAt: Date, downloadExpiresAt: Date, downloadURL: URL,
               createAt: Date, status: String) {
     self.type = type
     self.id = id
+    self.name = name
     self.resumble = resumble
     self.uploadURL = uploadURL
     self.uploadExpiresAt = uploadExpiresAt
@@ -42,6 +44,7 @@ extension ComplianceModel: Decodable {
   private enum CodingKeys: String, CodingKey {
     case type
     case id
+    case name
     case resumable
     case uploadURL = "upload_url"
     case uploadExpiresAt = "upload_expires_at"
@@ -57,25 +60,26 @@ extension ComplianceModel: Decodable {
     let typeRawValue = try values.decode(String.self, forKey: .type)
     self.type = .init(rawValue: typeRawValue)!
 
-    self.id = try values.decode(String.self, forKey: .id)
-    self.resumble = try values.decode(Bool.self, forKey: .resumable)
-    self.status = try values.decode(String.self, forKey: .status)
+    self.id = try values.decode(_.self, forKey: .id)
+    self.name = try values.decode(_.self, forKey: .name)
+    self.resumble = try values.decode(_.self, forKey: .resumable)
+    self.status = try values.decode(_.self, forKey: .status)
 
     let uploadURL = try values.decode(String.self, forKey: .uploadURL)
     self.uploadURL = .init(string: uploadURL)!
     
-    let downloadURL = try values.decode(String.self, forKey: .downloadURL)
+    let downloadURL: String = try values.decode(_.self, forKey: .downloadURL)
     self.downloadURL = .init(string: downloadURL)!
     
     let formatter = TwitterDateFormatter()
 
-    let uploadExpiresAt = try values.decode(String.self, forKey: .uploadExpiresAt)
+    let uploadExpiresAt: String = try values.decode(_.self, forKey: .uploadExpiresAt)
     self.uploadExpiresAt = formatter.date(from: uploadExpiresAt)!
 
-    let downloadExpiresAt = try values.decode(String.self, forKey: .downloadExpiresAt)
+    let downloadExpiresAt: String = try values.decode(_.self, forKey: .downloadExpiresAt)
     self.downloadExpiresAt = formatter.date(from: downloadExpiresAt)!
 
-    let createdAt = try values.decode(String.self, forKey: .createdAt)
+    let createdAt: String = try values.decode(_.self, forKey: .createdAt)
     self.createdAt = formatter.date(from: createdAt)!
   }
 }
