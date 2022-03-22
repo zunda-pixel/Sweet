04c2c8b726dd6628c604513b4035ffc8613b9c33
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-03-22 22:00:11 +0900

    SpacesResponseでusersを持つように

--- Sources/Sweet/Response/SpaceResponse.swift
+++ Sources/Sweet/Response/SpaceResponse.swift
@@ -10,12 +10,31 @@ import Foundation
 extension Sweet {
   public struct SpacesResponse {
     public let spaces: [SpaceModel]
+    public let users: [UserModel]
   }
 }
 
 extension Sweet.SpacesResponse: Decodable {
-   private enum CodingKeys: String, CodingKey {
-     case spaces = "data"
+  private enum CodingKeys: String, CodingKey {
+    case spaces = "data"
+    case includes
+  }
+  private enum UserCodingKeys: String, CodingKey {
+    case users
+  }
+  
+  public init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
+    
+    self.spaces = try values.decode([Sweet.SpaceModel].self, forKey: .spaces)
+    
+    guard let includes = try? values.nestedContainer(keyedBy: UserCodingKeys.self, forKey: .includes) else {
+      self.users = []
+      return
+    }
+    
+    let users = try? includes.decode([Sweet.UserModel].self, forKey: .users)
+    self.users = users ?? []
   }
 }
 
@@ -27,9 +46,9 @@ extension Sweet {
 }
 
 extension Sweet.SpaceResponse: Decodable {
-   private enum CodingKeys: String, CodingKey {
-     case space = "data"
-     case includes
+  private enum CodingKeys: String, CodingKey {
+    case space = "data"
+    case includes
   }
   
   private enum UserCodingKeys: String, CodingKey {
