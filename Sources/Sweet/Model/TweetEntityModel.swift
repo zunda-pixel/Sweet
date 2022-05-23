c6c83c227efc384cdf1f714f60ea2be746eda0e2
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-05-23 13:13:33 +0900

    EntityModelをUserとTweetで分けるように

--- Sources/Sweet/Model/TweetEntityModel.swift
+++ Sources/Sweet/Model/TweetEntityModel.swift
@@ -2,19 +2,19 @@
 //  EntityModel.swift
 //  
 //
-//  Created by zunda on 2022/03/13.
+//  Created by zunda on 2022/05/23.
 //
 
 import Foundation
 
 extension Sweet {
-  public struct EntityModel: Hashable {
+  public struct TweetEntityModel: Hashable {
     public let annotations: [AnnotationModel]
     public let urls: [URLModel]
     public let hashTags: [HashTagModel]
     public let mentions: [MentionModel]
     public let cashTags: [CashTagModel]
-    
+
     public init(annotations: [AnnotationModel] = [], urls: [URLModel] = [], hashTags: [HashTagModel] = [],
                 mentions: [MentionModel] = [], cashTags: [CashTagModel] = []) {
       self.annotations = annotations
@@ -26,7 +26,7 @@ extension Sweet {
   }
 }
 
-extension Sweet.EntityModel: Codable {
+extension Sweet.TweetEntityModel: Codable {
   private enum CodingKeys: String, CodingKey {
     case annotations
     case urls
@@ -34,26 +34,26 @@ extension Sweet.EntityModel: Codable {
     case mentions
     case cashTags
   }
-  
+
   public init(from decoder: Decoder) throws {
     let values = try decoder.container(keyedBy: CodingKeys.self)
-    
-    let annotations = try? values.decode([AnnotationModel].self, forKey: .annotations)
+
+    let annotations = try? values.decode([Sweet.AnnotationModel].self, forKey: .annotations)
     self.annotations = annotations ?? []
-    
-    let urls = try? values.decode([URLModel].self, forKey: .urls)
+
+    let urls = try? values.decode([Sweet.URLModel].self, forKey: .urls)
     self.urls = urls ?? []
-    
-    let hashTags = try? values.decode([HashTagModel].self, forKey: .hashTags)
+
+    let hashTags = try? values.decode([Sweet.HashTagModel].self, forKey: .hashTags)
     self.hashTags = hashTags ?? []
-    
-    let mentions = try? values.decode([MentionModel].self, forKey: .mentions)
+
+    let mentions = try? values.decode([Sweet.MentionModel].self, forKey: .mentions)
     self.mentions = mentions ?? []
-    
-    let cashTags = try? values.decode([CashTagModel].self, forKey: .cashTags)
+
+    let cashTags = try? values.decode([Sweet.CashTagModel].self, forKey: .cashTags)
     self.cashTags = cashTags ?? []
   }
-  
+
   public func encode(to encoder: Encoder) throws {
     var container = encoder.container(keyedBy: CodingKeys.self)
     try container.encode(annotations, forKey: .annotations)
@@ -64,36 +64,36 @@ extension Sweet.EntityModel: Codable {
   }
 }
 
-extension Sweet.EntityModel {
+extension Sweet {
   public struct HashTagModel: Hashable {
     public let start: Int
     public let end: Int
     public let tag: String
-    
+
     public init(start: Int, end: Int, tag: String) {
       self.start = start
       self.end = end
       self.tag = tag
     }
   }
-  
+
   public struct CashTagModel: Hashable {
     public let start: Int
     public let end: Int
     public let tag: String
-    
+
     public init(start: Int, end: Int, tag: String) {
       self.start = start
       self.end = end
       self.tag = tag
     }
   }
-  
+
   public struct MentionModel: Hashable {
     public let start: Int
     public let end: Int
     public let userName: String
-    
+
     public init(start: Int, end: Int, userName: String) {
       self.start = start
       self.end = end
@@ -102,19 +102,18 @@ extension Sweet.EntityModel {
   }
 }
 
-extension Sweet.EntityModel.HashTagModel: Codable {
-  
+extension Sweet.HashTagModel: Codable {
+
 }
 
-extension Sweet.EntityModel.CashTagModel: Codable {
-  
+extension Sweet.CashTagModel: Codable {
+
 }
 
-extension Sweet.EntityModel.MentionModel: Codable {
+extension Sweet.MentionModel: Codable {
   private enum CodingKeys: String, CodingKey {
     case start
     case end
     case userName = "username"
   }
 }
-
---
+++ Sources/Sweet/Model/UserEntityModel.swift
@@ -0,0 +1,85 @@
+//
+//  EntityModel.swift
+//  
+//
+//  Created by zunda on 2022/03/13.
+//
+
+import Foundation
+
+extension Sweet {
+  public struct UserEntityModel: Hashable {
+    public let urls: [URLModel]
+    public let descriptionURLs: [URLModel]
+    public let hashTags: [HashTagModel]
+    public let mentions: [MentionModel]
+    public let cashTags: [CashTagModel]
+
+    public init(urls: [URLModel] = [], descriptionURLs: [URLModel], hashTags: [HashTagModel] = [],
+                mentions: [MentionModel] = [], cashTags: [CashTagModel] = []) {
+      self.urls = urls
+
+      self.descriptionURLs = descriptionURLs
+      self.hashTags = hashTags
+      self.mentions = mentions
+      self.cashTags = cashTags
+    }
+  }
+}
+
+
+extension Sweet.UserEntityModel: Codable {
+  private enum CodingKeys: String, CodingKey {
+    case url
+    case description
+  }
+
+  private enum URLCodingKeys: String, CodingKey {
+    case urls
+  }
+
+  private enum DescriptionCodingKeys: String, CodingKey {
+    case urls
+    case hashTags = "hashtags"
+    case mentions
+    case cashTags = "cashtags"
+  }
+
+  public init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
+
+    let urlData = try? values.nestedContainer(keyedBy: URLCodingKeys.self, forKey: .url)
+
+    let urls = try? urlData?.decode([Sweet.URLModel].self, forKey: .urls)
+
+    self.urls = urls ?? []
+
+    let descriptionData = try? values.nestedContainer(keyedBy: DescriptionCodingKeys.self, forKey: .description)
+
+    let descriptionURLs = try? descriptionData?.decode([Sweet.URLModel].self, forKey: .urls)
+
+    self.descriptionURLs = descriptionURLs ?? []
+
+    let hashTags = try? descriptionData?.decode([Sweet.HashTagModel].self, forKey: .hashTags)
+    self.hashTags = hashTags ?? []
+
+    let mentions = try? descriptionData?.decode([Sweet.MentionModel].self, forKey: .mentions)
+    self.mentions = mentions ?? []
+
+    let cashTags = try? descriptionData?.decode([Sweet.CashTagModel].self, forKey: .cashTags)
+    self.cashTags = cashTags ?? []
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+
+    var urlContainer = container.nestedContainer(keyedBy: URLCodingKeys.self, forKey: .url)
+    try urlContainer.encode(urls, forKey: .urls)
+
+    var descriptionContainer = container.nestedContainer(keyedBy: DescriptionCodingKeys.self, forKey: .description)
+    try descriptionContainer.encode(descriptionURLs, forKey: .urls)
+    try descriptionContainer.encode(hashTags, forKey: .hashTags)
+    try descriptionContainer.encode(mentions, forKey: .mentions)
+    try descriptionContainer.encode(cashTags, forKey: .cashTags)
+  }
+}
