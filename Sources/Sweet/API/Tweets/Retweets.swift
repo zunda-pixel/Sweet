a9eefb7ab10a05d8f1fb1133c29ca79c28fe6a9b
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-06-27 17:52:00 +0900

    ドキュメント通りの実装だったが、正しく修正されたので、コメントアウト削除

--- Sources/Sweet/API/Tweets/Retweets.swift
+++ Sources/Sweet/API/Tweets/Retweets.swift
@@ -16,9 +16,6 @@ extension Sweet {
     
     let queries: [String: String?] = [
       UserField.key: userFields.map(\.rawValue).joined(separator: ","),
-      //PlaceField.key: placeFields.map(\.rawValue).joined(separator: ","),
-      //MediaField.key: mediaFields.map(\.rawValue).joined(separator: ","),
-      //PollField.key: pollFields.map(\.rawValue).joined(separator: ","),
       TweetField.key: tweetFields.map(\.rawValue).joined(separator: ","),
       Expansion.key: allUserExpansion.joined(separator: ","),
       "pagination_token": paginationToken,
