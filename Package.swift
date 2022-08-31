20af51ce16bf108816b35505608720a64fe28aa3
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-08-31 23:31:52 +0900

    不要なコメントを削除

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -7,20 +6,15 @@ var package = Package(
     name: "Sweet",
     platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Sweet",
             targets: ["Sweet"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
       .package(url: "https://github.com/zunda-pixel/HTTPClient", .upToNextMajor(from: "1.3.3")),
       .package(url: "https://github.com/apple/swift-format", .upToNextMajor(from: "0.50600.1")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Sweet",
             dependencies: [
