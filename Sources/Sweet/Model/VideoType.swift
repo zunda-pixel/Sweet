9fe239c1ed8aa28089bc9960c41e81693e8491dd
zunda <zunda.pixel@gmail.com>
2022-09-21 12:51:55 +0900

    StringをVideoTypeに変更

---
+++ Sources/Sweet/Model/VideoType.swift
@@ -0,0 +1,15 @@
+//
+//  VideoType.swift
+//  
+//
+//  Created by zunda on 2022/09/21.
+//
+
+import Foundation
+
+extension Sweet {
+  public enum VideoType: String, Sendable {
+    case x_mpegURL = "application/x-mpegURL"
+    case mp4 = "video/mp4"
+  }
+}
