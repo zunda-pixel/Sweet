5cbfbb03084f958d9fa4a5555805d87ef8993cb1
zunda <zunda.pixel@gmail.com>
2022-10-10 05:32:01 +0900

    ComplianceModelを実装

--- Sources/Sweet/Model/ComplianceModel.swift
+++ Sources/Sweet/Model/ComplianceModel.swift
@@ -1,95 +1,49 @@
 //
 //  ComplianceModel.swift
-//  
-//
-//  Created by zunda on 2022/01/14.
 //
 
 import Foundation
 
 extension Sweet {
-  /// Compliance Model
-  public struct ComplianceModel: Hashable, Identifiable, Sendable {
+  /// ComplianceModel
+  public struct ComplianceModel: Sendable {
     public let id: String
-    public let name: String
+    public let action: ComplianceAction
     public let createdAt: Date
-    public let type: JobType
-    public let resumable: Bool
-    public let uploadURL: URL
-    public let uploadExpiresAt: Date
-    public let downloadURL: URL
-    public let downloadExpiresAt: Date
-    public let status: String
-    
-    public init(id: String, name: String, createdAt: Date, type: JobType, resumable: Bool,
-                uploadURL: URL, uploadExpiresAt: Date, downloadExpiresAt: Date, downloadURL: URL,status: String) {
-      self.id = id
-      self.name = name
-      self.createdAt = createdAt
-      self.type = type
-      self.resumable = resumable
-      self.uploadURL = uploadURL
-      self.uploadExpiresAt = uploadExpiresAt
-      self.downloadURL = downloadURL
-      self.downloadExpiresAt = downloadExpiresAt
-      self.status = status
-    }
+    public let reason: ComplianceReason
   }
 }
 
 extension Sweet.ComplianceModel: Codable {
   private enum CodingKeys: String, CodingKey {
-    case type
     case id
-    case name
-    case resumable
-    case uploadURL = "upload_url"
-    case uploadExpiresAt = "upload_expires_at"
-    case downloadExpiresAt = "download_expires_at"
-    case downloadURL = "download_url"
+    case action
     case createdAt = "created_at"
-    case status
+    case reason
   }
   
   public init(from decoder: Decoder) throws {
-    let values = try decoder.container(keyedBy: CodingKeys.self)
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    self.id = try container.decode(String.self, forKey: .id)
     
-    let typeRawValue = try values.decode(String.self, forKey: .type)
-    self.type = .init(rawValue: typeRawValue)!
+    let action = try container.decode(String.self, forKey: .action)
+    self.action = .init(rawValue: action)!
     
-    self.id = try values.decode(String.self, forKey: .id)
-    self.name = try values.decode(String.self, forKey: .name)
-    self.resumable = try values.decode(Bool.self, forKey: .resumable)
-    self.status = try values.decode(String.self, forKey: .status)
+    let createdAt = try container.decode(String.self, forKey: .createdAt)
+    self.createdAt = Sweet.TwitterDateFormatter().date(from: createdAt)!
     
-    let uploadURL = try values.decode(String.self, forKey: .uploadURL)
-    self.uploadURL = .init(string: uploadURL)!
-    
-    let downloadURL: String = try values.decode(String.self, forKey: .downloadURL)
-    self.downloadURL = .init(string: downloadURL)!
-    
-    let formatter = Sweet.TwitterDateFormatter()
-    
-    let uploadExpiresAt: String = try values.decode(String.self, forKey: .uploadExpiresAt)
-    self.uploadExpiresAt = formatter.date(from: uploadExpiresAt)!
-    
-    let downloadExpiresAt: String = try values.decode(String.self, forKey: .downloadExpiresAt)
-    self.downloadExpiresAt = formatter.date(from: downloadExpiresAt)!
-    
-    let createdAt: String = try values.decode(String.self, forKey: .createdAt)
-    self.createdAt = formatter.date(from: createdAt)!
+    let reason = try container.decode(String.self, forKey: .reason)
+    self.reason = .init(rawValue: reason)!
   }
   
   public func encode(to encoder: Encoder) throws {
     var container = encoder.container(keyedBy: CodingKeys.self)
-    try container.encode(type.rawValue, forKey: .type)
-    try container.encode(name, forKey: .name)
     try container.encode(id, forKey: .id)
-    try container.encode(resumable, forKey: .resumable)
-    try container.encode(uploadURL, forKey: .uploadURL)
-    try container.encode(uploadExpiresAt, forKey: .uploadExpiresAt)
-    try container.encode(downloadURL, forKey: .downloadURL)
+    try container.encode(action.rawValue, forKey: .action)
+    
+    let createdAt = Sweet.TwitterDateFormatter().string(from: createdAt)
     try container.encode(createdAt, forKey: .createdAt)
-    try container.encode(status, forKey: .status)
+    
+    try container.encode(reason.rawValue, forKey: .reason)
   }
 }
