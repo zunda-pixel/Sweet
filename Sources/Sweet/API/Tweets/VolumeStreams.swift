ee4738ebf3af1029ae1b15ee1aa17dc66d534bd9
zunda <zunda.pixel@gmail.com>
2022-10-09 18:30:00 +0900

    queriesを初期化してしまうバグがあったので修正

--- Sources/Sweet/API/Tweets/VolumeStreams.swift
+++ Sources/Sweet/API/Tweets/VolumeStreams.swift
@@ -23,25 +23,26 @@ extension Sweet {
     
     let url: URL = .init(string: "https://api.twitter.com/2/tweets/sample/stream")!
     
-    let queries: [String: String?] = [
+    var queries: [String: String?] = [
       TweetField.key: tweetFields.map(\.rawValue).joined(separator: ","),
       UserField.key: userFields.map(\.rawValue).joined(separator: ","),
       PlaceField.key: placeFields.map(\.rawValue).joined(separator: ","),
       MediaField.key: mediaFields.map(\.rawValue).joined(separator: ","),
       PollField.key: pollFields.map(\.rawValue).joined(separator: ","),
       Expansion.key: allTweetExpansion.joined(separator: ","),
-    ].filter { $0.value != nil && $0.value != ""}
+    ]
+    
+    if let backfillMinutes {
+      queries["backfill_minutes"] = String(backfillMinutes)
+    }
+    
+    let emptyRemovedQueries = queries.filter { $0.value != nil && $0.value != ""}
     
     let headers = getBearerHeaders(type: .App)
     
     var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
-    components.queryItems = queries.map { .init(name: $0, value: $1)}
+    components.queryItems = emptyRemovedQueries.map { .init(name: $0, value: $1)}
     
-    if let backfillMinutes {
-      let queries = ["backfill_minutes": String(backfillMinutes)]
-      components.queryItems = queries.map { .init(name: $0, value: $1) }
-    }
-
     var request = URLRequest(url: components.url!)
     request.allHTTPHeaderFields = headers
     
