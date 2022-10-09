5aa02f69a9c99e80fcab7f873e3ab96eb630f84c
zunda <zunda.pixel@gmail.com>
2022-10-10 08:46:34 +0900

    propertiesを削除

--- Sources/Sweet/Model/GeoModel.swift
+++ Sources/Sweet/Model/GeoModel.swift
@@ -8,13 +8,11 @@ extension Sweet {
   /// Geo Model
   public struct GeoModel: Sendable, Hashable {
     public let type: String
-    public let bbox: [String]
-    public let properties: [String]
+    public let bbox: [Double]
     
-    public init(type: String, bbox: [String] = [], properties: [String] = []) {
+    public init(type: String, bbox: [Double] = []) {
       self.type = type
       self.bbox = bbox
-      self.properties = properties
     }
   }
 }
