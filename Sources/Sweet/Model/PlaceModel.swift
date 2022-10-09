6d68eb7028d11983c1f2bf391b233a3751afbfa5
zunda <zunda.pixel@gmail.com>
2022-10-10 08:09:49 +0900

    各プロパティを追加

--- Sources/Sweet/Model/PlaceModel.swift
+++ Sources/Sweet/Model/PlaceModel.swift
@@ -9,20 +9,42 @@ import Foundation
 
 extension Sweet {
   /// Place Model
+  /// https://developer.twitter.com/en/docs/twitter-api/data-dictionary/object-model/place
   public struct PlaceModel: Hashable, Identifiable, Sendable {
     public let id: String
-    public let name: String
-
-    public init(id: String, name: String) {
+    public let fullName: String
+    public let name: String?
+    public let country: String?
+    public let countryCode: String?
+    public let geo: GeoModel?
+    public let placeType: String?
+    public let containedWithin: [String]
+    
+    public init(id: String, fullName: String, name: String? = nil, country: String? = nil, countryCode: String?, geo: GeoModel? = nil, placeType: String? = nil, containedWithin: [String] = []) {
       self.id = id
+      self.fullName = fullName
       self.name = name
+      self.country = country
+      self.countryCode = countryCode
+      self.geo = geo
+      self.placeType = placeType
+      self.containedWithin = containedWithin
     }
   }
 }
 
 extension Sweet.PlaceModel: Codable {
-  private enum CodingKeys: String, CodingKey {
-    case name = "full_name"
-    case id
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: Sweet.PlaceField.self)
+    self.id = try container.decode(String.self, forKey: .id)
+    self.fullName = try container.decode(String.self, forKey: .fullName)
+    self.name = try? container.decode(String.self, forKey: .name)
+    self.country = try? container.decode(String.self, forKey: .country)
+    self.countryCode = try? container.decode(String.self, forKey: .countryCode)
+    self.geo = try? container.decode(Sweet.GeoModel.self, forKey: .geo)
+    self.placeType = try? container.decode(String.self, forKey: .placeType)
+    
+    let containedWithin = try? container.decode([String].self, forKey: .containedWithin)
+    self.containedWithin = containedWithin ?? []
   }
 }
