dfdf0fa1dbcd49330a8cb32da3ec497dd4e57110
zunda <zunda.pixel@gmail.com>
2022-10-10 08:48:15 +0900

    CoordinateModelを削除

--- Sources/Sweet/Model/SimpleGeo.swift
+++ Sources/Sweet/Model/SimpleGeo.swift
@@ -10,11 +10,9 @@ extension Sweet {
     public var id: String { placeID }
 
     public let placeID: String
-    public let coordinate: CoordinateModel
     
-    public init(placeID: String, coordinate: CoordinateModel) {
+    public init(placeID: String) {
       self.placeID = placeID
-      self.coordinate = coordinate
     }
   }
 }
@@ -22,6 +20,5 @@ extension Sweet {
 extension Sweet.SimpleGeoModel: Codable {
   private enum CodingKeys: String, CodingKey {
     case placeID = "place_id"
-    case coordinate = "coordinates"
   }
 }
