a955537b9039aeaba14a4b251529f37576879179
zunda <zunda.pixel@gmail.com>
2022-10-10 06:55:29 +0900

    OAuth2系のAPIのためにOAuth2(struct)を実装

---
+++ Sources/Sweet/API/Authorization/OAuth2Model.swift
@@ -0,0 +1,21 @@
+//
+//  OAuth2Model.swift
+//
+
+import Foundation
+
+extension Sweet {
+  public struct OAuth2Model {
+    public let bearerToken: String
+    public let refreshToken: String
+    public let expiredSeconds: Int
+  }
+}
+
+extension Sweet.OAuth2Model: Decodable {
+  private enum CodingKeys: String, CodingKey {
+    case bearerToken = "access_token"
+    case refreshToken = "refresh_token"
+    case expiredSeconds = "expires_in"
+  }
+}
