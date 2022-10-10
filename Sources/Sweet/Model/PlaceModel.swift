6ddd4c5af454f180a61a8bf517e80208d7aef581
zunda <zunda.pixel@gmail.com>
2022-10-10 13:08:19 +0900

    デフォルトnilに

--- Sources/Sweet/Model/PlaceModel.swift
+++ Sources/Sweet/Model/PlaceModel.swift
@@ -20,7 +20,7 @@ extension Sweet {
     public let type: PlaceType?
     public let containedWithin: [String]
     
-    public init(id: String, fullName: String, name: String? = nil, country: String? = nil, countryCode: String?, geo: GeoModel? = nil, type: PlaceType? = nil, containedWithin: [String] = []) {
+    public init(id: String, fullName: String, name: String? = nil, country: String? = nil, countryCode: String? = nil, geo: GeoModel? = nil, type: PlaceType? = nil, containedWithin: [String] = []) {
       self.id = id
       self.fullName = fullName
       self.name = name
