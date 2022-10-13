f22dce751fb3d3cb0456aa2b9ea6983fd3b3516e
zunda <zunda.dev@gmail.com>
2022-10-13 15:38:44 +0900

    Sendableを各クラスに準拠させる

---
+++ Sources/Sweet/Other/Sendable++.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+#if os(Linux) || os(Windows)
+  import FoundationNetworking
+
+  extension Data: @unchecked Sendable { }
+  extension URLResponse: @unchecked Sendable { }
+  extension Date: @unchecked Sendable { }
+  extension URL: @unchecked Sendable { }
+  extension CGSize: @unchecked Sendable { }
+  extension URLSessionConfiguration: @unchecked Sendable { }
+#endif
+
