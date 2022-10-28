09d27d7fc84d146d95a6c59c8bf7c3348b76e299
zunda <zunda.dev@gmail.com>
2022-10-28 16:52:38 +0900

    public付与

--- Sources/Sweet/Other/DirectMessageEventType.swift
+++ Sources/Sweet/Other/DirectMessageEventType.swift
@@ -5,7 +5,7 @@
 import Foundation
 
 extension Sweet {
-  enum DirectMessageEventType: String, Sendable {
+  public enum DirectMessageEventType: String, Sendable {
     case messageCreate = "MessageCreate"
     case participatnsJoin = "ParticipantsJoin"
     case participantsLeave = "ParticipantsLeave"
