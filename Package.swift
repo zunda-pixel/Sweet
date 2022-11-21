8cac9f5e93e193953e27ca41eb4a1b501aeb7817
zunda <zunda.dev@gmail.com>
2022-11-21 22:25:11 +0900

    format

--- Package.swift
+++ Package.swift
@@ -3,30 +3,30 @@
 import PackageDescription
 
 var package = Package(
-    name: "Sweet",
-    platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
-    products: [
-        .library(
-            name: "Sweet",
-            targets: ["Sweet"]),
-    ],
-    dependencies: [
-      .package(url: "https://github.com/zunda-pixel/HTTPClient", .upToNextMajor(from: "1.3.3")),
-      .package(url: "https://github.com/apple/swift-format", branch: "main"),
-      .package(url: "https://github.com/zunda-pixel/OAuth1", .upToNextMajor(from: "1.0.0"))
-    ],
-    targets: [
-        .target(
-            name: "Sweet",
-            dependencies: [
-              .product(name: "HTTPClient", package: "HTTPClient"),
-              .product(name: "OAuth1", package: "OAuth1")
-            ]),
-        .testTarget(
-            name: "SweetTests",
-            dependencies: ["Sweet"],
-            path: "Tests",
-            resources: [.process("Resources")]
-        )
-    ]
+  name: "Sweet",
+  platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
+  products: [
+    .library(
+      name: "Sweet",
+      targets: ["Sweet"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/zunda-pixel/HTTPClient", .upToNextMajor(from: "1.3.3")),
+    .package(url: "https://github.com/apple/swift-format", branch: "main"),
+    .package(url: "https://github.com/zunda-pixel/OAuth1", .upToNextMajor(from: "1.0.0"))
+  ],
+  targets: [
+    .target(
+      name: "Sweet",
+      dependencies: [
+        .product(name: "HTTPClient", package: "HTTPClient"),
+        .product(name: "OAuth1", package: "OAuth1")
+      ]),
+    .testTarget(
+      name: "SweetTests",
+      dependencies: ["Sweet"],
+      path: "Tests",
+      resources: [.process("Resources")]
+    )
+  ]
 )
