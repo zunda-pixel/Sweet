3f000f1c5fb8192165aebf9ae53a00589d9588ca
zunda <zunda.dev@gmail.com>
2022-12-07 15:31:45 +0900

    Move Expansion -> Sweet.Expansion

--- Sources/Sweet/API/Expansions/Expansion.swift
+++ Sources/Sweet/API/Expansions/Expansion.swift
@@ -1,40 +1,9 @@
 //
 //  Expansion.swift
 //
-//
-//  Created by zunda on 2022/01/31.
-//
-
-public struct Expansion: Sendable {
-  static var key: String = "expansions"
-}
 
 extension Sweet {
-  var allTweetExpansion: [String] {
-    let allExpansions: [String] =
-      tweetExpansions.map(\.rawValue) + pollExpansions.map(\.rawValue)
-      + mediaExpansions.map(\.rawValue) + placeExpansions.map(\.rawValue)
-    return allExpansions
-  }
-
-  var allUserExpansion: [String] {
-    let allExpansions: [String] = userExpansions.map(\.rawValue)
-    return allExpansions
-  }
-
-  var allDirectMessageExpansion: [String] {
-    let allExpansions: [String] =
-      directMesssageExpansions.map(\.rawValue) + mediaExpansions.map(\.rawValue)
-    return allExpansions
-  }
-
-  var allListExpansion: [String] {
-    let allExpansions: [String] = listExpansions.map(\.rawValue)
-    return allExpansions
-  }
-
-  var allSpaceExpansion: [String] {
-    let allExpansions: [String] = spaceExpansions.map(\.rawValue)
-    return allExpansions
+  struct Expansion: Sendable {
+    static let key: String = "expansions"
   }
 }
--- Sources/Sweet/Sweet.swift
+++ Sources/Sweet/Sweet.swift
@@ -39,3 +39,33 @@ public struct Sweet: Sendable {
     self.config = config
   }
 }
+
+extension Sweet {
+  var allTweetExpansion: [String] {
+    let allExpansions: [String] =
+      tweetExpansions.map(\.rawValue) + pollExpansions.map(\.rawValue)
+      + mediaExpansions.map(\.rawValue) + placeExpansions.map(\.rawValue)
+    return allExpansions
+  }
+
+  var allUserExpansion: [String] {
+    let allExpansions: [String] = userExpansions.map(\.rawValue)
+    return allExpansions
+  }
+
+  var allDirectMessageExpansion: [String] {
+    let allExpansions: [String] =
+      directMesssageExpansions.map(\.rawValue) + mediaExpansions.map(\.rawValue)
+    return allExpansions
+  }
+
+  var allListExpansion: [String] {
+    let allExpansions: [String] = listExpansions.map(\.rawValue)
+    return allExpansions
+  }
+
+  var allSpaceExpansion: [String] {
+    let allExpansions: [String] = spaceExpansions.map(\.rawValue)
+    return allExpansions
+  }
+}
