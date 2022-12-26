1321d94fee0507a48a4de3c3f65b05ca7ca146d3
zunda <zunda.dev@gmail.com>
2022-12-27 00:02:24 +0900

    add to 7

--- Tests/SweetTests/TestCodableModel.swift
+++ Tests/SweetTests/TestCodableModel.swift
@@ -213,13 +213,12 @@ final class TestCodableModel: XCTestCase {
   }
 
   func testJSONFileDecodable() throws {
-    for i in (1..<4) {
+    for i in (1..<7) {
       let path = Bundle.module.path(forResource: "TweetsData\(i)", ofType: "json")!
       let rawString = try String(contentsOfFile: path)
       let rawData = rawString.data(using: .utf8)!
 
-      let response = try JSONDecoder.twitter.decode(Sweet.TweetsResponse.self, from: rawData)
-      print(response)
+      _ = try JSONDecoder.twitter.decode(Sweet.TweetsResponse.self, from: rawData)
     }
   }
 }
