4ab409171766d8634c54ac77715b71644681fb16
zunda <zunda.dev@gmail.com>
2023-01-30 21:17:01 +0900

    Add AutorizationError

---
+++ Sources/Sweet/Authorization/AuthorizationError.swift
@@ -0,0 +1,37 @@
+//
+//  AuthorizationError.swift
+//
+
+import Foundation
+
+extension Sweet {
+  public enum AuthorizationError: Error {
+    case invalidClient
+    case invalidRequest
+    case unknown(response: AuthorizationErrorResponse)
+  }
+}
+
+extension Sweet {
+  public struct AuthorizationErrorResponse: Decodable {
+    let error: String
+    let errorDescription: String
+    
+    var authorizationError: Sweet.AuthorizationError {
+      if error == "invalid_client" {
+        return .invalidClient
+      }
+      
+      if error == "invalid_request" {
+        return .invalidRequest
+      }
+      
+      return .unknown(response: self)
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+      case error
+      case errorDescription = "error_description"
+    }
+  }
+}
