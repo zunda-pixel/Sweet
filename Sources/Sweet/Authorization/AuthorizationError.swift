969e928cf55b20bc6c6a051e7145e429e6aadfa5
zunda <zunda.dev@gmail.com>
2023-02-08 14:50:48 +0900

    format

--- Sources/Sweet/Authorization/AuthorizationError.swift
+++ Sources/Sweet/Authorization/AuthorizationError.swift
@@ -16,19 +16,19 @@ extension Sweet {
   public struct AuthorizationErrorResponse: Decodable {
     public let error: String
     public let errorDescription: String
-    
+
     var authorizationError: Sweet.AuthorizationError {
       if error == "invalid_client" {
         return .invalidClient
       }
-      
+
       if error == "invalid_request" {
         return .invalidRequest
       }
-      
+
       return .unknown(response: self)
     }
-    
+
     private enum CodingKeys: String, CodingKey {
       case error
       case errorDescription = "error_description"
